aa23efb7e7554e5dc9a97ded4a2e8d3a7e485769
yp <you@example.com>
2022-11-30 10:43:05 +0800

    ch07: add basic interpreter (calculator++)
    currently only evaluates expressions and prints value

---
+++ java/com/craftinginterpreters/lox/Interpreter.java
@@ -0,0 +1,148 @@
+package com.craftinginterpreters.lox;
+
+class Interpreter implements Expr.Visitor<Object> {
+
+  void interpret(Expr expression) {
+    try {
+      Object value = evaluate(expression);
+      System.out.println(stringify(value));
+    } catch (RuntimeError error) {
+      Lox.runtimeError(error);
+    }
+  }
+
+  /*
+  literals: hardcoded value found in the code
+. - comes from the parser’s domain
+
+  values: produced by computation
+  - are an interpreter concept, part of the runtime’s world
+  - NOT literals
+  */
+  @Override
+  public Object visitLiteralExpr(Expr.Literal expr) {
+    return expr.value;
+  }
+
+  @Override
+  public Object visitGroupingExpr(Expr.Grouping expr) {
+    // evaluate sub-expression before returning it
+    return evaluate(expr.expression);
+  }
+
+  @Override
+  public Object visitUnaryExpr(Expr.Unary expr) {
+    // evaluate sub-expression before applying operator
+    Object right = evaluate(expr.right);
+
+    switch (expr.operator.type) {
+      case BANG:
+        return !isTruthy(right);
+      case MINUS:
+        checkNumberOperand(expr.operator, right);
+        return -(double)right;
+    }
+
+    // Unreachable.
+    return null;
+  }
+
+  @Override
+  public Object visitBinaryExpr(Expr.Binary expr) {
+    // operands are evauted left to right
+    // visible to the user if opearnds have side-effects
+    Object left = evaluate(expr.left);
+    Object right = evaluate(expr.right);
+
+    switch (expr.operator.type) {
+      case GREATER:
+        checkNumberOperands(expr.operator, left, right);
+        return (double)left > (double)right;
+      case GREATER_EQUAL:
+        checkNumberOperands(expr.operator, left, right);
+        return (double)left >= (double)right;
+      case LESS:
+        checkNumberOperands(expr.operator, left, right);
+        return (double)left < (double)right;
+      case LESS_EQUAL:
+        checkNumberOperands(expr.operator, left, right);
+        return (double)left <= (double)right;
+      case BANG_EQUAL:
+        return !isEqual(left, right);
+      case EQUAL_EQUAL:
+        return isEqual(left, right);
+      case MINUS:
+        checkNumberOperands(expr.operator, left, right);
+        return (double)left - (double)right;
+      case PLUS:
+        if (left instanceof Double && right instanceof Double) {
+          return (double)left + (double)right;
+        }
+        if (left instanceof String && right instanceof String) {
+          return (String)left + (String)right;
+        }
+        throw new RuntimeError(expr.operator,
+            "Operands must be two numbers or two strings.");
+      case SLASH:
+        checkNumberOperands(expr.operator, left, right);
+        return (double)left / (double)right;
+      case STAR:
+        checkNumberOperands(expr.operator, left, right);
+        return (double)left * (double)right;
+    }
+
+    // Unreachable.
+    return null;
+  }
+
+  // only false and nil are falsey
+  // everything else is truthy
+  private boolean isTruthy(Object object) {
+    if (object == null) return false;
+    if (object instanceof Boolean) return (boolean)object;
+    return true;
+  }
+
+  private boolean isEqual(Object a, Object b) {
+    if (a == null && b == null) return true;
+    // ensure no NullPointerException on a.equals()
+    if (a == null) return false;
+    // reuse Java’s equals() for Boolean, Double, String etc
+    return a.equals(b);
+  }
+
+  private String stringify(Object object) {
+    if (object == null) return "nil";
+
+    if (object instanceof Double) {
+      String text = object.toString();
+     // remove decimal point for integers
+     if (text.endsWith(".0")) {
+        text = text.substring(0, text.length() - 2);
+      }
+      return text;
+    }
+
+    return object.toString();
+  }
+
+  private void checkNumberOperand(Token operator, Object operand) {
+    if (operand instanceof Double) return;
+    throw new RuntimeError(operator, "Operand must be a number.");
+  }
+
+  private void checkNumberOperands(Token operator,
+                                   Object left, Object right) {
+    if (left instanceof Double && right instanceof Double) return;
+    throw new RuntimeError(operator, "Operands must be numbers.");
+  }
+
+  /*
+  helper method
+  - calls accept() method
+  - dispatched back to visit*() methods in Interpreter class
+  */
+  private Object evaluate(Expr expr) {
+    return expr.accept(this);
+  }
+}
